331d411be608e8ef59b6c4790f428951718f13b5
seamlik <seamlikok@gmail.com>
2024-03-05 18:36:48 +0100

    Test serving many requests

--- main/src/server.rs
+++ main/src/server.rs
@@ -125,13 +125,13 @@ mod test {
     use std::io::ErrorKind;
 
     #[tokio::test]
-    async fn serve() -> anyhow::Result<()> {
-        let multicast_address: SocketAddrV6 = "[::A]:1".parse()?;
+    async fn serve() {
+        let multicast_address: SocketAddrV6 = "[::A]:1".parse().unwrap();
         let request = Request {
             request_id: "REQUEST".into(),
             response_collector_port: 3,
         };
-        let request_address = "[::123]:2".parse()?;
+        let request_address = "[::123]:2".parse().unwrap();
 
         let expected_response = Response {
             request_id: request.request_id.clone(),
@@ -172,35 +172,30 @@ mod test {
             .return_const::<Vec<_>>(expected_response.response_id.clone());
 
         // when
-        let e = serve_internal(
+        let result = serve_internal(
             multicast_address,
             [1, 2],
-            expected_response.port.try_into()?,
+            expected_response.port.try_into().unwrap(),
             multicast_receiver,
             response_sender,
             response_id_generator,
         )
-        .await
-        .unwrap_err();
+        .await;
 
         // Then
-        assert_eq!(
-            ErrorKind::Other,
-            e.kind(),
-            "Server must exist with the dummy error supplied at the end of all requests"
-        );
-
-        Ok(())
+        assert_server_exits_with_dummy_error(result);
     }
 
     #[tokio::test]
-    async fn failing_to_handle_packet_does_not_stop_serving() -> anyhow::Result<()> {
-        let mut multicast_receiver = MockMulticastReceiver::default();
+    async fn failing_to_handle_packet_does_not_stop_serving() {
+        let request_source_address = "[::123]:2".parse().unwrap();
         let mut requests = [
-            Ok((vec![], "[::123]:2".parse()?)),
+            Ok((vec![], request_source_address)),
             Err(ErrorKind::Other.into()),
         ]
         .into_iter();
+
+        let mut multicast_receiver = MockMulticastReceiver::default();
         multicast_receiver
             .expect_join_multicast()
             .return_once_st(|_, _| Ok(()));
@@ -214,30 +209,68 @@ mod test {
             .return_once(|_, _| anyhow::bail!("Failed to send response"));
 
         // when
-        let e = serve_internal(
-            "[::A]:1".parse()?,
+        let result = serve_internal(
+            "[::A]:1".parse().unwrap(),
             [1],
             1,
             multicast_receiver,
             response_sender,
             UuidGenerator,
         )
-        .await
-        .unwrap_err();
+        .await;
 
         // Then
-        assert_eq!(
-            ErrorKind::Other,
-            e.kind(),
-            "Server must exist with the dummy error supplied at the end of all requests"
-        );
+        assert_server_exits_with_dummy_error(result);
+    }
 
-        Ok(())
+    #[tokio::test]
+    async fn handle_many_requests() {
+        let request_size = 128;
+        let request_source_address = "[::123]:2".parse().unwrap();
+        let mut requests = std::iter::repeat_with(move || Ok((vec![], request_source_address)))
+            .take(request_size)
+            .chain(std::iter::once(Err(ErrorKind::Other.into())));
+
+        let mut multicast_receiver = MockMulticastReceiver::default();
+        multicast_receiver
+            .expect_join_multicast()
+            .return_once_st(|_, _| Ok(()));
+        multicast_receiver
+            .expect_receive()
+            .returning(move || requests.next().unwrap());
+
+        let mut response_sender = MockResponseSender::default();
+        response_sender
+            .expect_send()
+            .times(request_size)
+            .returning(|_, _| Ok(()));
+
+        // when
+        let result = serve_internal(
+            "[::A]:1".parse().unwrap(),
+            [1],
+            1,
+            multicast_receiver,
+            response_sender,
+            UuidGenerator,
+        )
+        .await;
+
+        // Then
+        assert_server_exits_with_dummy_error(result);
     }
 
     #[test]
     fn response_id_has_reasonable_size() {
         let response_id = UuidGenerator.generate();
         assert!(response_id.len() <= 16);
+    }
+
+    fn assert_server_exits_with_dummy_error(result: std::io::Result<()>) {
+        assert_eq!(
+            ErrorKind::Other,
+            result.unwrap_err().kind(),
+            "Server must have exited with the dummy error supplied at the end of all requests"
+        );
     }
 }
